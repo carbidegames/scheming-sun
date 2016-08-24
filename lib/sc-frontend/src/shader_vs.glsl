d29b9c5fde9e40efa328f81e28f4155835ab8be0
Layl <LaylConway@users.noreply.github.com>
2016-08-25 00:15:26 +0200

    Rename shader values to personal preference

--- lib/sc-frontend/src/shader_vs.glsl
+++ lib/sc-frontend/src/shader_vs.glsl
@@ -3,10 +3,10 @@
 #extension GL_ARB_separate_shader_objects : enable
 #extension GL_ARB_shading_language_420pack : enable
 
-layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 normal;
+layout(location = 0) in vec3 i_position;
+layout(location = 1) in vec3 i_normal;
 
-layout(location = 0) out vec3 v_normal;
+layout(location = 0) out vec3 o_normal;
 
 layout(set = 0, binding = 0) uniform Data {
     mat4 worldview;
@@ -14,6 +14,6 @@ layout(set = 0, binding = 0) uniform Data {
 } uniforms;
 
 void main() {
-    v_normal = transpose(inverse(mat3(uniforms.worldview))) * normal;
-    gl_Position = uniforms.proj * uniforms.worldview * vec4(position, 1.0);
+    o_normal = transpose(inverse(mat3(uniforms.worldview))) * i_normal;
+    gl_Position = uniforms.proj * uniforms.worldview * vec4(i_position, 1.0);
 }
